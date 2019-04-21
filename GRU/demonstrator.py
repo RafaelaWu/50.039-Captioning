58958e16fb47d58f89687bd0d52df15897833aab
Li Xueqing <lixueqingsm19@gmail.com>
2019-04-21 16:43:59 +0800

    Add files via upload

---
+++ GRU/demonstrator.py
@@ -0,0 +1,39 @@
+import kivy
+import json
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
+import os
+import argparse
+
+import sample
+from sample import Vocabulary
+
+kivy.require('1.10.1')
+Window.size = (1000,600)
+
+presentation = Builder.load_file('demonstrator.kv')
+
+class MainScreen(BoxLayout):
+
+    def selected(self, filename):
+        self.ids.img.source = filename[0]
+
+        sentences = sample.run(self.ids.img.source, args.encoder_path, args.decoder_path)
+        for i in range(len(sentences)):
+        	sentences[i] = sentences[i][8:-6]
+
+        self.ids.pred.text = '\n'.join(sentences)
+        #self.ids.pred.text = 'guess what I have predicted'
+
+class MyApp(App):
+    def build(self):
+        return MainScreen()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--encoder_path', type=str, default='GRU128_encoder_epoch3.ckpt', help='path for trained encoder')
+    parser.add_argument('--decoder_path', type=str, default='GRU128_decoder_epoch3.ckpt', help='path for trained decoder')
+    args = parser.parse_args()
+    MyApp().run()
